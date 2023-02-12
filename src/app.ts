62a3d9d15ada3592e3585c1faeb224f49a31425f
Marcelo Cesar <marcelo.cesards@gmail.com>
2023-02-12 15:24:57 -0300

    separando comandos do terminal

--- src/app.ts
+++ src/app.ts
@@ -6,32 +6,7 @@ import chalk from 'chalk';
 import boxen, { BorderStyle } from 'boxen';
 import yargs from 'yargs/yargs';
 import terminal  from "./console";
+import commands from './commands';
 
-
-const greeting = chalk.white.bold("Hello!");
-
-const boxenOptions = {
-    padding: 1,
-    margin: 1,
-    borderStyle: BorderStyle.Round,
-    borderColor: "green",
-    backgroundColor: "#555555"
-};
-const msgBox = boxen(greeting, boxenOptions);
-
-console.log(msgBox);
-
-
-
-const parse = yargs(process.argv)
-    .option("n", { alias: "name", 
-    describe: "Your name", type: "string", demandOption: false })
-    ;
-
-(async () => {
-    const argv = await parse.argv;
-    argv.name // => No error, type: boolean
-    console.log(argv.name);
-})();
-
+commands.command();
 terminal.terminal();
---
+++ src/commands/index.ts
@@ -0,0 +1,34 @@
+import chalk from 'chalk';
+import boxen, { BorderStyle } from 'boxen';
+import yargs from 'yargs/yargs';
+
+function command() {
+    
+    const greeting = chalk.white.bold("Hello!");
+
+    const boxenOptions = {
+        padding: 1,
+        margin: 1,
+        borderStyle: BorderStyle.Round,
+        borderColor: "green",
+        backgroundColor: "#555555"
+    };
+    const msgBox = boxen(greeting, boxenOptions);
+
+    console.log(msgBox);
+
+
+
+    const parse = yargs(process.argv)
+        .option("n", { alias: "name", 
+        describe: "Your name", type: "string", demandOption: false })
+        ;
+
+    (async () => {
+        const argv = await parse.argv;
+        argv.name // => No error, type: boolean
+        console.log(argv.name);
+    })();
+}
+
+export default {command};
